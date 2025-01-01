81079b3c147953855b9d9e3510d074074766a0d8
Faraz Alam <farazalam706@gmail.com>
2025-01-01 21:57:35 +0500

    1stJan

--- app.js
+++ app.js
@@ -560,4 +560,473 @@
 
 // ----------------Without If else------------
 
-function isEven(num) {}
+// function isEven(num) {}
+
+// var faraz = {
+//   canMakeWebs: true,
+//   aGymer: true,
+//   smoke: false,
+// };
+
+// for (let key in faraz) {
+//   console.log(key, faraz[key]);
+// }
+
+// setTimeout(function () {
+//   console.log("Hello World!");
+// }, 2000);
+
+// var arr = [1, 2, 3, 4, 5, 6];
+
+// let newArr = arr.map((val) => {
+//   return val * 2;
+// });
+
+// console.log(newArr);
+
+// var arr = [1, 2, 3, 4, 5, 6];
+
+// let newArr = arr.forEach((val) => {
+//   console.log(val * 2);
+// });
+
+// var arr = [1, 2, 3, 4, 5, 6];
+
+// let newArr = arr.filter((val) => {
+//   return val % 2 == 0;
+// });
+
+// console.log("newArr: ", newArr);
+
+// test = async () => {
+//   console.log("1: Message");
+//   console.log("2: Message");
+//   await console.log("3: Message");
+//   console.log("4: Message");
+// };
+
+// test();
+// console.log("5: Message");
+
+// Create a function that takes in two strings as two parameters and returns a boolean that indicates whether
+// or not the first string is an anagram of the second string.
+// Anagram = An anagram is another word or phrase formed by rearranging the letters of the first word or
+// phrase.
+// (Sample Input : function(“pan”,”nap”))
+// (Sample Output : true)
+
+// function anagram(stringOne, stringTwo) {
+//   let string1 = stringOne.toLowerCase();
+//   let string2 = stringTwo.toLowerCase();
+
+//   if (string1.length !== string2.length) {
+//     return false;
+//   }
+//   return string1.split("").sort().join("") == string2.split("").sort().join("");
+// }
+// console.log(anagram("nap", "pan"));
+
+// Calculate the sum of numbers received in a comma-delimited string.
+// a. (Sample Input : "1.5, 2.3, 3.1, 4, 5.6, 6, 7, 8, 9, 10.9")
+// b. (Sample Output : 57.1)
+
+// let arr = ["1.5, 2.3, 3.1, 4, 5.6, 6, 7, 8, 9, 10.9"];
+
+// let sum = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//   sum = sum + arr[i];
+//   return sum;
+// }
+
+// console.log(sum);
+
+// function isPrime(number) {
+//   if (number < 2) return false;
+
+//   for (let i = 2; i <= Math.sqrt(number); i++) {
+//     if (number % i === 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// console.log(isPrime(11));
+
+// function isComposite(num) {
+//   if (num < 2) return false;
+
+//   for (let i = 2; i <= Math.sqrt(num); i++) {
+//     if (num % i === 0) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
+
+// console.log(isComposite(6));
+
+// Calculate the sum of numbers received in a comma-delimited string.
+// a. (Sample Input : "1.5, 2.3, 3.1, 4, 5.6, 6, 7, 8, 9, 10.9")
+// b. (Sample Output : 57.1)
+
+// let string = "1.5, 2.3, 3.1, 4, 5.6, 6, 7, 8, 9, 10.9";
+// let arr = string.split(",")
+// let arr2 = [];
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   arr2.push(Number(arr[i]));
+//   sum = sum + arr2[i];
+// }
+// console.log("arr2: ", arr2);
+// console.log("sum: ", sum);
+
+// function isOddEven(num) {
+//   if (num % 2 === 0) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// }
+// isOddEven(2);
+
+// function isWholeNumber(num) {
+//   if (num >= 0) {
+//     console.log("Whole Number!");
+//   } else {
+//     console.log("Not a Whole Number!");
+//   }
+// }
+
+// isWholeNumber(-7);
+
+// 1 2 3 4 5
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
+
+// for (let i = 5; i >= 1; i--) {
+//   for (let j = 1; j <= i; j++) {
+//     document.write(j + " ");
+//   }
+//   document.write("<br>");
+// }
+
+// 1
+// 2 2
+// 3 3 3
+// 4 4 4 4
+// 5 5 5 5 5
+
+// for (let i = 1; i <= 5; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     document.write(i + " ");
+//   }
+//   document.write("<br>");
+// }
+
+// 5 4 3 2 1
+// 4 3 2 1
+// 3 2 1
+// 2 1
+// 1
+
+// for (let i = 1; i <= 5; i++) {
+//   for (let j = 5; j >= i; j--) {
+//     document.write(j + " ");
+//   }
+//   document.write("<br>");
+// }
+
+// 1 2 3 4 5
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
+
+// for (let i = 5; i >= 1; i--) {
+//   for (let j = 1; j <= i; j++) {
+//     document.write(j + " ");
+//   }
+//   document.write("<br>");
+// }
+
+// 1
+// 1 1
+// 1 1 1
+// 1 1 1 1
+// 1 1 1 1 1
+// 1 1 1 1
+// 1 1 1
+// 1 1
+// 1
+
+// for (let i = 1; i <= 6; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     document.write(1 + " ");
+//   }
+//   document.write("<br>");
+// }
+
+// for (let i = 5; i >= 1; i--) {
+//   for (let j = 1; j <= i; j++) {
+//     document.write(1 + " ");
+//   }
+//   document.write("<br>");
+// }
+
+// 1 2 3 4 5
+//   2 3 4 5
+//    3 4 5
+//     4 5
+//      5
+
+// for (let i = 1; i <= 5; i++) {
+//   for (let j = i; j <= 5; j++) {
+//     document.write(j + " ");
+//   }
+//   document.write("<br>");
+// }
+
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+
+// for (let i = 1; i <= 5; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     document.write(j);
+//   }
+//   document.write("<br>");
+// }
+
+// Program to print matrix of any dimension
+
+// function matrix(r, c) {
+//   let matrix = [];
+
+//   for (let i = 0; i < r; i++) {
+//     let column = [];
+//     for (let j = 0; j < c; j++) {
+//       column.push(0);
+//     }
+//     matrix.push(column);
+//   }
+
+//   console.log(matrix);
+// }
+
+// matrix(5, 5);
+
+// Program to print determinant of a 2x2 matrix
+
+// let matrix = [
+//   [1, 2],
+//   [3, 4],
+// ];
+
+// let determinant = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+// console.log("determinant: ", determinant);
+
+// getline(cin, students[i]);
+
+// let a = 4;
+// let b = 5;
+// [a, b] = [b, a];
+// console.log(a);
+// console.log(b);
+
+// ---------------Bubble Sorting(Ascending Order)--------------------
+
+// let arr = [4, 2, 5, 1, 9, 3, 6, 7];
+
+// function sorting(arr) {
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     for (let j = 0; j < arr.length - 1; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+//       }
+//     }
+//   }
+//   return arr;
+// }
+
+// console.log("Sorted Array:", sorting(arr));
+
+// ---------------Bubble Sorting(Descending Order)--------------------
+
+// let arr = [4, 2, 5, 1, 9, 3, 6, 7];
+
+// function sorting(arr) {
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     for (let j = arr.length - 1; j > 0; j--) {
+//       if (arr[j] > arr[j - 1]) {
+//         [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
+//       }
+//     }
+//   }
+//   return arr;
+// }
+
+// console.log("Sorted Array:", sorting(arr));
+
+// --------------Linear Search--------------------
+
+// let arr = [4, 2, 5, 1, 9, 3, 6, 7];
+// let valueToBeSearch = 5;
+// let flag = false;
+
+// function linearSearch(val, arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (val == arr[i]) {
+//       console.log("Value found at Index:", i);
+//       flag = true;
+//     }
+//   }
+//   if (!flag) {
+//     console.log("Value not found!");
+//   }
+// }
+// linearSearch(valueToBeSearch, arr);
+
+// ------------------BinarySearch----------------------
+
+// let arr = [4, 2, 5, 1, 9, 3, 6, 7];
+// let toFind = 3;
+// arr.sort();
+// let flag = false;
+
+// function binarySearch(arr, toFind) {
+//   let startingIndex = 0;
+//   let endingIndex = arr.length - 1;
+//   while (startingIndex <= endingIndex) {
+//     let midIndex = Math.floor((startingIndex + endingIndex) / 2);
+
+//     if (toFind == arr[midIndex]) {
+//       return `Value found at Index: ${midIndex}`;
+//     } else if (arr[midIndex] > toFind) {
+//       endingIndex = midIndex - 1;
+//     } else if (arr[midIndex] < toFind) {
+//       startingIndex = midIndex + 1;
+//     }
+//   }
+
+//   return "Value not found!";
+// }
+
+// console.log(binarySearch(arr, toFind));
+
+// function isPrime(number) {
+//   if (number < 2) return false;
+
+//   for (let i = 2; i <= Math.sqrt(number); i++) {
+//     if (number % i === 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// console.log(isPrime(11));
+
+// --------------------Get nth prime number----------------------
+
+// function isPrime(number) {
+//   if (number < 2) return false;
+
+//   for (let i = 2; i <= Math.sqrt(number); i++) {
+//     if (number % i === 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// function findNthPrime(n) {
+//   if (n < 1) return "Invalid input"; // Handle edge cases
+
+//   let count = 0; // Number of primes found
+//   let num = 1; // Number to check for primality
+
+//   while (count < n) {
+//     num++;
+//     if (isPrime(num)) {
+//       count++;
+//     }
+//   }
+//   return num;
+// }
+
+// // Example: Find the 10th prime number
+// console.log(findNthPrime(10)); // Output: 29
+
+// -----------------Get n total prime numbers--------------------
+
+// function isPrime(number) {
+//   if (number < 2) return false;
+
+//   for (let i = 2; i <= Math.sqrt(number); i++) {
+//     if (number % i === 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// function getNPrimes(n) {
+//   if (n < 1) return "Invalid input"; // Handle edge cases
+
+//   let primes = []; // Array to store the primes
+//   let num = 2; // Start checking from 2
+
+//   while (primes.length < n) {
+//     if (isPrime(num)) {
+//       primes.push(num);
+//     }
+//     num++;
+//   }
+//   return primes;
+// }
+
+// // Example: Get the first 10 prime numbers
+// console.log(getNPrimes(10)); // Output: [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+
+// ----------------------------------------------------------
+
+// function charFrequency(str, char) {
+//   let count = 0;
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] === char) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+
+// // Example: Find the frequency of 'a' in the string "banana"
+// const str = "banana";
+// const char = "a";
+// console.log(`Frequency of '${char}' in "${str}":`, charFrequency(str, char)); // Output: 3
+
+// -----------------------Find frequency of all characters in the string-------------------------------
+
+// function charFrequencyAll(str) {
+//   let frequency = {};
+
+//   for (let char of str) {
+//     if (frequency[char]) {
+//       frequency[char]++;
+//     } else {
+//       frequency[char] = 1;
+//     }
+//   }
+
+//   return frequency;
+// }
+
+// // Example: Find the frequency of all characters in "banana"
+// const str = "banana";
+// console.log(charFrequencyAll(str));
